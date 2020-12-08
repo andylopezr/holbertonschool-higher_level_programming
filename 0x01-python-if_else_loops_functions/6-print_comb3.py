8c7064dbf7c8f7d6258c74998804383eb9c61a41
Andy Lopez <profesor.de.ingles@aol.com>
2020-12-08 18:12:08 -0500

    Prints all possible different combiinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(90):
+    if (i % 10 > i / 10):
+        if (i < 89):
+            print("{:02}, ".format(i), end='')
+        else:
+            print("{:02}".format(i))
