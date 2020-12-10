90ccb2d07d5bf1d04cc4bad6cba1f1dbf32d682e
Andy Lopez <profesor.de.ingles@aol.com>
2020-12-10 17:18:55 -0500

    Changed else position

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif op == ops[3]:
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
