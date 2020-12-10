d23009b56f87070e1e34e3234d020dc02add356a
Andy Lopez <profesor.de.ingles@aol.com>
2020-12-10 17:08:41 -0500

    Updated signs

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     ops = "+", "-", "*", "/"
     if op in ops:
         if op == ops[0]:
-            print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif op == ops[1]:
-            print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         elif op == ops[2]:
-            print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif op == ops[3]:
-            print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
