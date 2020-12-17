f83af6652ee6f12332df4f95eae2b807f5821c4b
Andy Lopez <profesor.de.ingles@aol.com>
2020-12-17 15:45:25 -0500

    Added condition to validate existance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    total = 0
-    prev = 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = [roman[x] for x in roman_string]
-    for x in numbers:
-        if x > prev:
-            total -= prev * 2
-        prev = x
-        total += x
-    return total
+    if isinstance(roman_string, str):
+        total = 0
+        prev = 0
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        numbers = [roman[x] for x in roman_string]
+        for x in numbers:
+            if x > prev:
+                total -= prev * 2
+            prev = x
+            total += x
+        return total
