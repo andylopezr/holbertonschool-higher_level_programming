152a82514f680f056091cdf252c2b5423f5d1b82
Andy Lopez <profesor.de.ingles@aol.com>
2020-12-17 15:47:41 -0500

    Updated condition to check for str

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str):
-        total = 0
-        prev = 0
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        numbers = [roman[x] for x in roman_string]
-        for x in numbers:
-            if x > prev:
-                total -= prev * 2
-            prev = x
-            total += x
-        return total
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    total = 0
+    prev = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    integer = [roman[x] for x in roman_string]
+    for x in integer:
+        if x > prev:
+            total -= prev * 2
+        prev = x
+        total += x
+    return total
