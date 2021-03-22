00416b3e0cf5a8fca9d02b32038abb42697604b5
Andy Lopez <profesor.de.ingles@aol.com>
2021-03-22 16:38:25 -0500

    ADDED | Script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function factorial (number) {
+  if (isNaN(number) || number <= 1) return 1;
+  return number * factorial(number - 1);
+}
+const number = parseInt(process.argv[2]);
+console.log(factorial(number));
