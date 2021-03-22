0e0bbc0f9ee03130076ba108f981fc59285cd5d8
Andy Lopez <profesor.de.ingles@aol.com>
2021-03-22 16:10:15 -0500

    Script that prints x times C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+if (isNaN(x)) console.log('Missing number of occurrences');
+else {
+  for (let c = 0; c < x; c++) console.log('C is fun');
+}
