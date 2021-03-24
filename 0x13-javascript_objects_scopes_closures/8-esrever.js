2938aabafe18e3b53b94bc76a16c0abc1cade723
Andy Lopez <profesor.de.ingles@aol.com>
2021-03-23 20:04:56 -0500

    FIXED | style

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 module.exports.esrever = function (list) {
-  let newlist = [];
+  const newlist = [];
   let c = 0;
   for (let l = list.length - 1; l >= 0; l--, c++) {
     newlist[c] = list[l];
